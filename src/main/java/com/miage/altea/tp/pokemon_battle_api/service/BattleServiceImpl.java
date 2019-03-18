add65419d0c8b60acf777e9bd1d897c62abb41eb
fabien vantorre <fabien.vantorre@hotmail.fr>
2019-03-18 09:48:04 +0100

    le premier joueur est celui qui a le plus de speed

--- src/main/java/com/miage/altea/tp/pokemon_battle_api/service/BattleServiceImpl.java
+++ src/main/java/com/miage/altea/tp/pokemon_battle_api/service/BattleServiceImpl.java
@@ -5,12 +5,12 @@ import com.miage.altea.tp.pokemon_battle_api.bo.Pokemon;
 import com.miage.altea.tp.pokemon_battle_api.bo.Trainer;
 import com.miage.altea.tp.pokemon_battle_api.exceptions.BattleFinishedException;
 import com.miage.altea.tp.pokemon_battle_api.exceptions.NotYourTurnException;
+import com.miage.altea.tp.pokemon_battle_api.repository.BattleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import com.miage.altea.tp.pokemon_battle_api.repository.BattleRepository;
 
 import java.util.List;
 
@@ -30,7 +30,6 @@ public class BattleServiceImpl implements BattleService {
     public void setTrainerServiceUrl(String trainerServiceUrl) {
         this.trainerServiceUrl = trainerServiceUrl;
     }
-
 
 
     @Autowired
@@ -53,23 +52,24 @@ public class BattleServiceImpl implements BattleService {
 
     @Override
     public Integer createBattle(String trainer1Name, String trainer2Name) {
-        Trainer trainer1= getTrainer(trainer1Name);
-        Trainer trainer2= getTrainer(trainer2Name);
+        Trainer trainer1 = getTrainer(trainer1Name);
+        Trainer trainer2 = getTrainer(trainer2Name);
         trainer1.getTeam().forEach(pokemon -> pokemon.setCurrentStates(pokemon.getPokemonTypeObject().getStats().toBuilder().build(), pokemon.getLevel()));
         trainer2.getTeam().forEach(pokemon -> pokemon.setCurrentStates(pokemon.getPokemonTypeObject().getStats().toBuilder().build(), pokemon.getLevel()));
-        return battleRepository.create(Battle.builder().currentTrainer(1).trainer1(trainer1).trainer2(trainer2).build());
+        boolean trainer1Start = trainer1.getTeam().get(0).getCurrentStates().getSpeed() < trainer2.getTeam().get(0).getCurrentStates().getSpeed();
+        return battleRepository.create(Battle.builder().currentTrainer((trainer1Start) ? 1 : 2).trainer1(trainer1).trainer2(trainer2).build());
     }
 
     @Override
     public Battle attack(Integer id, String trainerName) throws NotYourTurnException, BattleFinishedException {
         Battle battle = getBattle(id);
-        if(!itIsHisTurn(battle, trainerName)){
+        if (!itIsHisTurn(battle, trainerName)) {
             throw new NotYourTurnException("reviens plus tard");
         }
 
         Pokemon attaquant = getAttaquant(battle);
         Pokemon defenseur = getDefenseur(battle);
-        if(attaquant == null || defenseur == null){
+        if (attaquant == null || defenseur == null) {
             throw new BattleFinishedException("the battle is alreadyFinished");
         }
 
@@ -80,14 +80,14 @@ public class BattleServiceImpl implements BattleService {
     }
 
     private void doFight(Pokemon attaquant, Pokemon defenseur) {
-        Integer attaque = ((2 * attaquant.getLevel() / 5) + ( 2 * attaquant.getCurrentStates().getAttack() / defenseur.getCurrentStates().getDefense())) + 2;
+        Integer attaque = ((2 * attaquant.getLevel() / 5) + (2 * attaquant.getCurrentStates().getAttack() / defenseur.getCurrentStates().getDefense())) + 2;
         defenseur.getCurrentStates().setHp(defenseur.getCurrentStates().getHp() - attaque);
     }
 
     private boolean itIsHisTurn(Battle battle, String trainerName) {
         int current = battle.getCurrentTrainer();
-        if(current == 1 && trainerName.equals(battle.getTrainer1().getName())) return true;
-        if(current == 2 && trainerName.equals(battle.getTrainer2().getName())) return true;
+        if (current == 1 && trainerName.equals(battle.getTrainer1().getName())) return true;
+        if (current == 2 && trainerName.equals(battle.getTrainer2().getName())) return true;
         return false;
     }
 
@@ -101,7 +101,7 @@ public class BattleServiceImpl implements BattleService {
         return trainer.getTeam().stream().filter(pokemon -> pokemon.getCurrentStates().getHp() > 0).findFirst().orElse(null);
     }
 
-    private Trainer getTrainer(String name){
+    private Trainer getTrainer(String name) {
         Trainer res = this.trainerApiRestTemplate.getForObject(this.trainerServiceUrl + "/trainers/" + name, Trainer.class);
         res.getTeam().forEach(pokemon -> pokemon.setPokemonTypeObject(pokemonTypeService.getPokemonType(pokemon.getPokemonType())));
         return res;
